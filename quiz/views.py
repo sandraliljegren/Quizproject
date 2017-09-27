cd24b683ae77b76b2e228a911f8d457804fafd11
sandraliljegren <hello@sandraliljegren.com>
2017-09-27 11:59:07 +0200

    Added temporary data in start in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
+quizzes = [
+		{
+			"quiz_number": 1,
+			"name": "Klassiska böcker",
+			"description": "Hur bra kan du dina klassiker?"
+		},
+		{
+			"quiz_number": 2,
+			"name": "Största fotbollslagen",
+			"description": "Kan du dina lag?"
+		},
+		{
+			"quiz_number": 3,
+			"name": "Världens mest kända hackare",
+			"description": "Kan du din hackerhistoria?"
+		},
+]
+
 def start(request):
-	return render(request, "start.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "start.html", context)
 
 def quiz(request, quiz_number):
 	return render(request, "quiz.html")
