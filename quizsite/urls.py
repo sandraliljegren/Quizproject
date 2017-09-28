935669148f2aebd2c2571d9337dbbabbec017fa3
sandraliljegren <hello@sandraliljegren.com>
2017-09-28 11:35:33 +0200

    Admin up and running

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from quiz import views
+from django.contrib import admin
 
 urlpatterns = [
 		path("", views.start, name="start_page"),
 		path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 		path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
-		path("quiz/<int:quiz_number>/results/", views.results, name="results_page"),	
+		path("quiz/<int:quiz_number>/results/", views.results, name="results_page"),
+		path('admin/', admin.site.urls),	
 		]
